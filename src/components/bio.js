d7a9efc986d97d67744603dc7da2f2debe3363b9
rugieri <rugieri.ronaldo@gmail.com>
2021-05-17 00:11:40 -0300

    .

--- src/components/bio.js
+++ src/components/bio.js
@@ -46,8 +46,8 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+          <a href={`https://instagram.com/${social?.instagram || ``}`}>
+            You should follow them on Instagram
           </a>
         </p>
       )}
